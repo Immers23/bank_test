5080a52cafbc84083ac72f19e14335762d35f00a
Imraan Ali <imraan91@hotmail.co.uk>
2020-02-25 18:38:44 +0000

    refactored to create a bank_account object before tests automatically

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -11,19 +11,19 @@ class Bank_account
 
   def deposit(deposit_amount, date)
     @balance += deposit_amount
-    transaction = Transaction.new(date, ('%.2f' % deposit_amount).to_i, nil, '%.2f' % @balance)
+    transaction = Transaction.new(date, '%.2f' % deposit_amount.to_i, nil, '%.2f' % @balance)
     @transaction_history.unshift(transaction)
   end
 
   def withdraw(withdraw_amount, date)
     @balance -= withdraw_amount
-    transaction = Transaction.new(date, nil, ('%.2f' % withdraw_amount).to_i, '%.2f' % @balance)
+    transaction = Transaction.new(date, nil, '%.2f' % withdraw_amount.to_i, '%.2f' % @balance)
     @transaction_history.unshift(transaction)
   end
 
   def print_balance
     puts "date || credit || debit || balance"
-      @transaction_history.each do |transaction| puts "#{transaction.date} || #{transaction.deposit_amount} || #{transaction.withdraw_amount} || #{transaction.balance}"
+      @transaction_history.each do |transaction| puts"#{transaction.date} || #{transaction.deposit_amount} || #{transaction.withdraw_amount} || #{transaction.balance}"
     end
   end
 end
