3f82e002a2f925a44a7a92baa46e3c7ec6da278c
Imraan Ali <imraan91@hotmail.co.uk>
2020-02-25 14:44:08 +0000

    bank balance increaes by 1000 when amount is deposit

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,4 +6,8 @@ class Bank
     @balance = 0
   end
 
+  def deposit(amount)
+    @balance += amount
+  end
+
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,4 +5,10 @@ describe 'bank test features for withdrawing, depositing and printing balanace' 
     bank = Bank.new
     expect(bank.balance).to eq 0
   end
+
+  it 'will increase the balance by the amount deposited' do
+    bank = Bank.new
+    bank.deposit(1000)
+    expect(bank.balance).to eq 1000
+  end
 end
